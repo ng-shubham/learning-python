85832298a6217277a82fc538c1d616000066f574
ng-shubham <ngshubham01@gmail.com>
2026-02-02 15:35:55 +0530

    loops in python | Shubham Ambhore

---
+++ loops/loop01.py
@@ -0,0 +1,9 @@
+arr = [1,2,3,4,5,6,7,8]
+
+# for i in arr:
+#     print(i)
+
+i=1
+while i < 10:
+    print(i)
+    i += 1
---
+++ loops/sum.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+sum = 0
+i = 0
+while i <= num:
+    sum += i
+    i += 1
+print("The sum is:", sum)
