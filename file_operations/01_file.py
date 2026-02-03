75e92291fef83f8dbbe5a3cd1c4237773428b25d
ng-shubham <ngshubham01@gmail.com>
2026-02-03 14:16:00 +0530

    file operation in python | Shubham Ambhore

---
+++ file_operations/01_file.py
@@ -0,0 +1,4 @@
+f = open("test.txt", "r")
+content = f.read()
+print(content)
+f.close()
---
+++ file_operations/02_write.py
@@ -0,0 +1,5 @@
+str = "Hello, Python"
+
+w = open("file_operations/myfile.txt", "w")
+w.write(str)
+w.close()
